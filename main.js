041596f7a61396f9c1718e0fadf154152fb4e961
qorbaxk <bekgyber3@hanmail.net>
2022-04-22 02:11:40 +0900

    sizak

--- main.js
+++ main.js
@@ -1,13 +1,28 @@
+//지역
+
+//온도
+let nowTemp = 0;
+//체감온도
+let feelTemp = 0;
+//강수량
+let rainFall = 0;
+
+
 //날씨api 불러오는 함수
 const getWeather = async() =>{
-    let url= new URL(`https://api.openweathermap.org/data/2.5/weather?q=jeonju&units=metric&appid=ef710ba10aec5ee8c5ce8f984a15dff0`);
-    
-
+    let url= new URL(`https://api.openweathermap.org/data/2.5/weather?q=seoul&units=metric&appid=ef710ba10aec5ee8c5ce8f984a15dff0`);
     let response = await fetch(url);
-    
     let data = await response.json();
     console.log(data);
+
+    nowTemp = data.main.temp;
+    feelTemp = data.main.feels_like;
+    rainFall = data.rain;
     
+    console.log("현재온도는",Math.round(nowTemp),"°");
+    console.log("체감온도는",Math.round(feelTemp),"°");
+    console.log("강수량은",rainFall);
+
 
 }
 
