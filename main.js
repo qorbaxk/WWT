7e3bb82285933c1251469b0b5a1561f2b1bbe400
qorbaxk <bekgyber3@hanmail.net>
2022-04-23 00:15:21 +0900

    4/23 update

--- main.js
+++ main.js
@@ -1,6 +1,31 @@
 //멘트 정리
 let rainMent = '';
 let feelMent = '';
+let outerEmoji = '';
+let topcloEmoji = '';
+let btmcloEmoji = '';
+let outerMent = [
+    "목도리","장갑",
+    "롱패딩","겨울코트",
+    "숏패딩","무스탕",
+    "플리스","뽀글이",
+    "항공점퍼","트렌치코트","레더재킷",
+    "바람막이","아노락재킷",
+    "트러커 재킷","가디건","블레이저재킷","후드집업"
+];
+let topcloMent = [
+    "히트텍/내복","두꺼운니트","기모후드티","기모맨투맨",
+    "니트","후드티","맨투맨",
+    "셔츠","블라우스","롱슬리브",
+    "반팔티","반팔셔츠","민소매"
+];
+let btmcloMent = [
+    "히트텍/내복","기모바지","코듀로이바지",
+    "슬랙스","청바지","면바지","롱치마",
+    "린넨바지","냉장고바지","반바지","치마"
+];
+
+
 
 //지역
 let whereLoca = '';
@@ -19,7 +44,7 @@ let cloud = 0;
 
 //날씨api 불러오는 함수
 const getWeather = async() =>{
-    let url= new URL(`https://api.openweathermap.org/data/2.5/weather?&q=busan&units=metric&appid=ef710ba10aec5ee8c5ce8f984a15dff0`);
+    let url= new URL(`https://api.openweathermap.org/data/2.5/weather?&q=seoul&units=metric&appid=ef710ba10aec5ee8c5ce8f984a15dff0`);
     let response = await fetch(url);
     let data = await response.json();
     console.log(data);
@@ -63,6 +88,40 @@ const render = () =>{
     <p id="now-ondo">${nowTemp}°</p>`;
 
     document.getElementById("ondo-thread").innerHTML = ondoHTML;
+
+
+    //옷차림
+    let emojiMent = ["🧣🧤","🧥","🥼","👔","👕👚","👖","🩳"];
+    if(11<nowTemp){
+        outerEmoji = "🥼";
+    }else if(nowTemp<4){
+        outerEmoji = "🧣🧤🧥";
+    }
+    else if(4<nowTemp<=11){
+        outerEmoji = "🧥";
+    }
+
+
+
+    let outerHTML = '';
+    outerHTML = `<p>${outerEmoji}</p>
+    <p>패딩,코트</p>`;
+
+    document.querySelector(".outer").innerHTML = outerHTML;
+
+    let topcloHTML = '';
+    topcloHTML = `<p>👔</p>
+    <p>반팔,긴팔</p>`;
+
+    document.querySelector(".topclo").innerHTML = topcloHTML;
+
+    let btmcloHTML = '';
+    btmcloHTML = `<p>👖</p>
+    <p>반바지,치마</p>`;
+
+    document.querySelector(".btmclo").innerHTML = btmcloHTML;
+
+
 
 
     //강수량 보여주기
@@ -123,7 +182,6 @@ const render = () =>{
 
 
 
-
 //지역
 function categoryChange(e) {
     const state = document.getElementById("state");
@@ -142,6 +200,7 @@ function categoryChange(e) {
     const jeonnam = ["군/구 선택","광양시","나주시","목포시","순천시","여수시","강진군","고흥군","곡성군","구례군","담양군","무안군","보성군","신안군","영광군","영암군","완도군","장성군","장흥군","진도군","함평군","해남군","화순군"];
     const jeonbuk = ["군/구 선택","군산시", "김제시", "남원시", "익산시", "전주시", "정읍시", "고창군", "무주군", "부안군", "순창군", "완주군", "임실군", "장수군", "진안군"];
     const jeju = ["군/구 선택","서귀포시","제주시","남제주군","북제주군"];
+    const chungnam = ["군/구 선택","공주시","계룡시","논산시","보령시","서산시","아산시","천안시","금산군","당진군","부여군","서천군","예산군","청양군","태안군","홍성군"];
     const chungbuk = ["군/구 선택","제천시","청주시","충주시","괴산군","단양군","보은군","영동군","옥천군","음성군","증평군","진천군","청원군"];
     
     if (e.value == "general01") {
