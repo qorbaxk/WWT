30734fe70f0f2c465489a213c97a8f21338ca959
qorbaxk <bekgyber3@hanmail.net>
2022-04-25 23:44:11 +0900

    4/25 update

--- main.js
+++ main.js
@@ -52,7 +52,7 @@ let middleTemp = 0;
 
 //날씨api 불러오는 함수
 const getWeather = async() =>{
-    let local = "Singapore";
+    let local = "seoul";
     let url= new URL(`https://api.openweathermap.org/data/2.5/weather?&q=${local}&units=metric&appid=ef710ba10aec5ee8c5ce8f984a15dff0`);
     let response = await fetch(url);
     let data = await response.json();
