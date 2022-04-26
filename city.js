c3d32f09184ab73c40c44eb5a6eb020f0a2803b8
qorbaxk <bekgyber3@hanmail.net>
2022-04-26 19:31:12 +0900

    local update

---
+++ city.js
@@ -0,0 +1,5 @@
+const localList = [
+    {name:"강남구", city:"Gangnam-gu"},
+    {name:"강동구", city:"Gangdong-gu"},
+    {name:"강북구", city:"Gangbuk-gu"}
+];
--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+
+
 //멘트 정리
 let rainMent = '';
 let feelMent = '';
@@ -8,6 +10,8 @@ let cloMent = '';
 let cloMent2 = '';
 let nowEmoji='';
 let ondoMent = '';
+
+let nowWhere = 'seoul';
 
 
 let outerMent = [
@@ -34,7 +38,7 @@ let btmcloMent = [
 
 
 //지역
-let whereLoca = '';
+let whereLoca = '서울';
 //온도
 let nowTemp = 0;
 //체감온도
@@ -50,9 +54,11 @@ let cloud = 0;
 //중간기온
 let middleTemp = 0;
 
+
+
 //날씨api 불러오는 함수
 const getWeather = async() =>{
-    let local = "seoul";
+    let local = `${nowWhere}`;
     let url= new URL(`https://api.openweathermap.org/data/2.5/weather?&q=${local}&units=metric&appid=ef710ba10aec5ee8c5ce8f984a15dff0`);
     let response = await fetch(url);
     let data = await response.json();
@@ -68,7 +74,7 @@ const getWeather = async() =>{
     }
     
     cloud = data.clouds.all;
-    whereLoca = data.name;
+    
     minTemp = Math.round(data.main.temp_min);
     maxTemp = Math.round(data.main.temp_max);
     middleTemp = Math.round((maxTemp+minTemp)/2);
@@ -91,6 +97,9 @@ getWeather();
 //보여주는 함수
 const render = () =>{
    
+
+
+    
   //날씨 이모지 보여주기
   if(rainFall>0){
     nowEmoji="🌧"
@@ -109,7 +118,7 @@ const render = () =>{
     //지역,현재온도 보여주기
     let ondoHTML = '';
 
-    ondoHTML = `<p id="now-city">${whereLoca}은 지금!</p> 
+    ondoHTML = `<p id="now-city">${whereLoca}</p> 
     <div class="containerbackground">${nowEmoji}</div>
     <p id="now-ondo">${nowTemp}°</p>
     <p id="highrow-tem">  ${minTemp}° / ${maxTemp}° </p>`;
@@ -237,43 +246,28 @@ const render = () =>{
 
 
 
+//지역고르기
 
+let nameValue = document.getElementById('state');
+let doValue = document.getElementById('loca-input');
+let cityName = '';
+let doName = '';
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//지역
+//상위>하위 지역 선택 및 상위지역 값 추출
 function categoryChange(e) {
     const state = document.getElementById("state");
   
-    const gangwon = ["군/구 선택","강릉시","동해시","삼척시","속초시","원주시","춘천시","태백시","고성군","양구군","양양군","영월군","인제군","정선군","철원군","평창군","홍천군","화천군","횡성군"];
-    const gyeonggi = ["군/구 선택","고양시","과천시","광명시","광주시","구리시","군포시","김포시","남양주시","동두천시","부천시","성남시","수원시","시흥시","안산시","안성시","안양시","양주시","오산시","용인시","의왕시","의정부시","이천시","파주시","평택시","포천시","하남시","화성시","가평군","양평군","여주군","연천군"];
-    const gyeongsangnam = ["군/구 선택","거제시", "김해시", "마산시", "밀양시", "사천시", "양산시", "진주시", "진해시", "창원시", "통영시", "거창군", "고성군", "남해군", "산청군", "의령군", "창녕군", "하동군", "함안군", "함양군", "합천군"];
-    const gyeongsangbuk = ["군/구 선택","경산시","경주시","구미시","김천시","문경시","상주시","안동시","영주시","영천시","포항시","고령군","군위군","봉화군","성주군","영덕군","영양군","예천군","울릉군","울진군","의성군","청도군","청송군","칠곡군"];
-    const gwangju = ["군/구 선택","광산구", "남구", "동구", "북구", "서구"];
-    const daegu = ["군/구 선택","남구", "달서구", "동구", "북구", "서구", "수성구", "중구", "달성군"];
-    const daejeon = ["군/구 선택","대덕구", "동구", "서구", "유성구", "중구"];
-    const busan = ["군/구 선택","강서구","금정구","남구","동구","동래구","부산진구","북구","사상구","사하구","서구","수영구","연제구","영도구","중구","해운대구","기장군"];
-    const seoul = ["군/구 선택","강남구","강동구","강북구","강서구","관악구","광진구","구로구","금천구","노원구","도봉구","동대문구","동작구","마포구","서대문구","서초구","성동구","성북구","송파구","양천구","영등포구","용산구","은평구","종로구","중구","중랑구"];
-    const ulsan = ["군/구 선택","남구","동구","북구","중구","울주군"];
-    const incheon = ["군/구 선택","계양구","남구","남동구","동구","부평구","서구","연수구","중구","강화군","옹진군"];
-    const jeonnam = ["군/구 선택","광양시","나주시","목포시","순천시","여수시","강진군","고흥군","곡성군","구례군","담양군","무안군","보성군","신안군","영광군","영암군","완도군","장성군","장흥군","진도군","함평군","해남군","화순군"];
-    const jeonbuk = ["군/구 선택","군산시", "김제시", "남원시", "익산시", "전주시", "정읍시", "고창군", "무주군", "부안군", "순창군", "완주군", "임실군", "장수군", "진안군"];
-    const jeju = ["군/구 선택","서귀포시","제주시","남제주군","북제주군"];
-    const chungnam = ["군/구 선택","공주시","계룡시","논산시","보령시","서산시","아산시","천안시","금산군","당진군","부여군","서천군","예산군","청양군","태안군","홍성군"];
-    const chungbuk = ["군/구 선택","제천시","청주시","충주시","괴산군","단양군","보은군","영동군","옥천군","음성군","증평군","진천군","청원군"];
+    const nothing = [""];
+    const gangwon = ["시/군 선택","강릉시","동해시","삼척시","속초시","원주시","춘천시","태백시","고성군","양구군","양양군","영월군","인제군","정선군","철원군","평창군","홍천군","화천군","횡성군"];
+    const gyeonggi = ["시/군 선택","고양시","과천시","광명시","광주시","구리시","군포시","김포시","남양주시","동두천시","부천시","성남시","수원시","시흥시","안산시","안성시","안양시","양주시","오산시","용인시","의왕시","의정부시","이천시","파주시","평택시","포천시","하남시","화성시","가평군","양평군","여주군","연천군"];
+    const gyeongsangnam = ["시/군 선택","거제시", "김해시", "마산시", "밀양시", "사천시", "양산시", "진주시", "진해시", "창원시", "통영시", "거창군", "고성군", "남해군", "산청군", "의령군", "창녕군", "하동군", "함안군", "함양군", "합천군"];
+    const gyeongsangbuk = ["시/군 선택","경산시","경주시","구미시","김천시","문경시","상주시","안동시","영주시","영천시","포항시","고령군","군위군","봉화군","성주군","영덕군","영양군","예천군","울릉군","울진군","의성군","청도군","청송군","칠곡군"];
+    const seoul = ["구 선택","강남구","강동구","강북구","강서구","관악구","광진구","구로구","금천구","노원구","도봉구","동대문구","동작구","마포구","서대문구","서초구","성동구","성북구","송파구","양천구","영등포구","용산구","은평구","종로구","중구","중랑구"];
+    const jeonnam = ["시/군 선택","광양시","나주시","목포시","순천시","여수시","강진군","고흥군","곡성군","구례군","담양군","무안군","보성군","신안군","영광군","영암군","완도군","장성군","장흥군","진도군","함평군","해남군","화순군"];
+    const jeonbuk = ["시/군 선택","군산시", "김제시", "남원시", "익산시", "전주시", "정읍시", "고창군", "무주군", "부안군", "순창군", "완주군", "임실군", "장수군", "진안군"];
+    const jeju = ["시/군 선택","서귀포시","제주시"];
+    const chungnam = ["시/군 선택","공주시","계룡시","논산시","보령시","서산시","아산시","천안시","금산군","당진군","부여군","서천군","예산군","청양군","태안군","홍성군"];
+    const chungbuk = ["시/군 선택","제천시","청주시","충주시","괴산군","단양군","보은군","영동군","옥천군","음성군","증평군","진천군","청원군"];
     
     if (e.value == "general01") {
         add = gangwon;
@@ -284,19 +278,19 @@ function categoryChange(e) {
       } else if (e.value == "general04") {
         add = gyeongsangbuk;
       } else if (e.value == "general05") {
-        add = gwangju;
+        add = nothing;
       } else if (e.value == "general06") {
-        add = daegu;
+        add = nothing;
       } else if (e.value == "general07") {
-        add = daejeon;
+        add = nothing;
       } else if (e.value == "general08") {
-        add = busan;
+        add = nothing;
       } else if (e.value == "general09") {
         add = seoul;
       } else if (e.value == "general10") {
-        add = ulsan;
+        add = nothing;
       } else if (e.value == "general11") {
-        add = incheon;
+        add = nothing;
       } else if (e.value == "general12") {
         add = jeonnam;
       } else if (e.value == "general13") {
@@ -308,18 +302,60 @@ function categoryChange(e) {
       } else if (e.value == "general16") {
         add = chungbuk;
       } 
+ 
+        
+      //하위도시 없을때
+      doName = doValue.options[doValue.selectedIndex].text;
+      console.log("큰지역은",doName);
+      changeCity();
+      
+
       //입력후 초기화
       state.options.length = 0;
+
+
       
       // 군/구 갯수;
       for (property in add) {
-		let opt = document.createElement("option");
-		opt.value = add[property];
-		opt.innerHTML = add[property];
-		state.appendChild(opt);
-	}
+        let opt = document.createElement("option");
+        opt.value = add[property];
+        opt.innerHTML = add[property];
+        state.appendChild(opt);
+	    }
+}
+
+//하위지역 값 추출
+const citySelect = () =>{
+
+  cityName = nameValue.options[nameValue.selectedIndex].text;
+  console.log(`도시는 ${nameValue.options[nameValue.selectedIndex].text}`);
+  if(cityName == ""){
+
+  }else{
+    changeCity();
+  }
+  
 }
 
 
 
+//지역 바꾸기
+const changeCity = () =>{
+  
+  //광역시 일때
+
+  
+  //작은도시 일때
+  for(i=0; i<localList.length; i++){
+    if(cityName == localList[i].name){
+      nowWhere = localList[i].city;
+      whereLoca = localList[i].name;
+      break;
+    }
+  }
+  console.log(nowWhere);
+  getWeather();
+  
+    
+}
 
